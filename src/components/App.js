7edd8f72f7c4bbf65b1207bd01221569303ef8f1
JHNC99 <jonathanhnc75@gmail.com>
2021-11-30 16:26:30 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/global.scss";
+const App = () => {
+  return (
+    <div>
+      <h1>Boilerplate Reactapp</h1>
+      <span>Author:</span> <small>Jonathan Hernandez</small>
+    </div>
+  );
+};
+
+export default App;
